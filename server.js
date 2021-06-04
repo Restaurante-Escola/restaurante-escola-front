dd7206edf9cac1bf058881d9d2e5211d4e8c0df1
Luis Durante <luisdurante2@outlook.com>
2021-06-04 00:36:04 -0300

    update deploy files

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/school-restaurant'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/school-restaurant/'}),
 );
 
 // Start the app by listening on the default Heroku port
