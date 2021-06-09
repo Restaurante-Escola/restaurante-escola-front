3c6ca6fd22288a6a74b7298f2c924c74cef06a10
Jaime Mathias de Lara Bueno <jaimemathias@hotmail.com>
2021-06-08 21:56:07 -0300

    implement getAlunosByTurmaCodigo

--- src/app/main/class-frequency/class-frequency.service.ts
+++ src/app/main/class-frequency/class-frequency.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClassFrequencyService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+ 
+  getAlunosByTurmaCodigo(codigoTurma: number) {
+    return this.httpClient.get(`${environment.api_url}/turmas/alunos/` + codigoTurma)
+  }
 }
