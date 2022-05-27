2d17f395d8affa088a08dab8dd84d0bad38cdbfe
Eduardo Neves <eduventuraneves@gmail.com>
2022-05-26 21:31:21 -0300

    Rota documentos e Environment localhost:8080

---
+++ src/app/main/documents/documents.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import * as moment from 'moment';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocumentsService {
+
+  constructor() { }
+}
